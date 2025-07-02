74aa9e4d9bb2b724f7879906652a5442fe9faf0a
Leko <leko@leko.moe>
2025-07-02 22:22:30 +0800

    Update API URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var userId = "152757524";
 var userServer = "netease";
 var userType = "playlist";
-window.meting_api = "https://api.qijieya.cn/meting/?server=:server&type=:type&id=:id&r=:r"
+window.meting_api = "https://api.obdo.cc/meting/?server=:server&type=:type&id=:id&r=:r"
